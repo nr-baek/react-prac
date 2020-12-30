7dabee0e02e9a6e4cbf1b5c134dda42c7dcbd610
NaraBaek <nara130825@gmail.com>
2020-12-30 23:10:34 +0900

    스타일링 컴포넌트 실습

---
+++ styling-react/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import CSSModule from "./components/CSSModule";
+import StyledComponent from "./components/StyledComponent";
+// import SassComponent from "./components/SassComponent";
+
+function App() {
+  return (
+    <div>
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
