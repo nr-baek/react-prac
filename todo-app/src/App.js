282d4926bb006bdcca6c5a2dfa5334b68a0b6034
NaraBaek <nara130825@gmail.com>
2020-12-31 12:53:50 +0900

    컴포넌트 기능 구현

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,54 @@
+import { useCallback, useState, useRef } from 'react';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      setTodos([...todos, todo]);
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
