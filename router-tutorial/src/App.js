30850b85e663076bc46ab2b81be4a0931b07c025
NaraBaek <nara130825@gmail.com>
2020-12-31 18:44:06 +0900

    리액트 라우트사용 실습 프로젝트 생성

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
