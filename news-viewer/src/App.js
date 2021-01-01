e3c52ff575bbd7f31c8d14ec90e0b5c15271852d
NaraBaek <nara130825@gmail.com>
2021-01-01 12:03:17 +0900

    외부API연동 실습 프로젝트 생성

---
+++ news-viewer/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+// import axios from 'axios';
+//a4cfe1e61f894d438a24d8be00504399
+import NewsList from './components/NewsList';
+
+const App = () => {
+  return <NewsList />;
+};
+
+export default App;
