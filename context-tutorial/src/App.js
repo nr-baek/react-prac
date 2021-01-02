423a75e452957cbda18a1aacc40beafed55f0986
NaraBaek <nara130825@gmail.com>
2021-01-02 11:16:12 +0900

    Context API 실습 프로젝트 생성

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
+
+function App() {
+  return (
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
+  );
+}
+
+export default App;
