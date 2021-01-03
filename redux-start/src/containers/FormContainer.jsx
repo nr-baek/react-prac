222d0e4892bda669e8561b9db62a802ac65c6e47
NaraBaek <nara130825@gmail.com>
2021-01-03 17:59:58 +0900

    hooks를 이용한 connect방식으로 코드 변경

--- redux-start/src/containers/FormContainer.jsx
+++ redux-start/src/containers/FormContainer.jsx
@@ -1,16 +1,22 @@
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addTodo } from '../actions';
 import Form from '../components/Form';
 
 // Container
 
-const FormContainer = connect(null, (dispatch) => {
-  return {
-    add: (todo) => {
-      dispatch(addTodo(todo));
-      console.log(todo);
-    },
-  };
-})(Form);
+// const FormContainer = connect(null, (dispatch) => ({
+//     add: (todo) => {
+//       dispatch(addTodo(todo));
+//       console.log(todo);
+//     }}))(Form);
+
+const FormContainer = () => {
+  const dispatch = useDispatch();
+  function add(todo) {
+    dispatch(addTodo(todo));
+  }
+  return <Form add={add} />;
+};
 
 export default FormContainer;
--- redux-start/src/containers/TodoListContainer.jsx
+++ redux-start/src/containers/TodoListContainer.jsx
@@ -1,16 +1,27 @@
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { completeTodo } from '../actions';
 import TodoList from '../components/TodoList';
 
-const TodoListContainer = connect(
-  (state) => ({ todos: state.todos }),
-  (dispatch) => {
-    return {
-      complete: (index) => {
-        dispatch(completeTodo(index));
-      },
-    };
-  },
-)(TodoList);
+// const TodoListContainer = connect(
+//   (state) => ({ todos: state.todos }),
+//   (dispatch) => {
+//     return {
+//       complete: (index) => {
+//         dispatch(completeTodo(index));
+//       },
+//     };
+//   },
+// )(TodoList);
+
+const TodoListContainer = () => {
+  const todos = useSelector((state) => state.todos);
+
+  const dispatch = useDispatch();
+  function complete(index) {
+    dispatch(completeTodo(index));
+  }
+  return <TodoList todos={todos} complete={complete} />;
+};
 
 export default TodoListContainer;
