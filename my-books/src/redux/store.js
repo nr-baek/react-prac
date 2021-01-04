683edb028c25d073e79ef5c8743d33b8feeedd1f
NaraBaek <nara130825@gmail.com>
2021-01-04 11:31:18 +0900

    미들웨어 실습 준비

--- my-books/src/redux/store.js
+++ my-books/src/redux/store.js
@@ -1,6 +1,35 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+//
+function middleware1(store) {
+  return (next) => {
+    console.log('middleware1', 1);
+    return (action) => {
+      console.log('middleware1', 2);
+      const returnValue = next(action);
+      console.log('middleware1', 3);
+      return returnValue;
+    };
+  };
+}
+
+function middleware2(store) {
+  return (next) => {
+    console.log('middleware2', 1);
+    return (action) => {
+      console.log('middleware2', 2);
+      const returnValue = next(action);
+      console.log('middleware2', 3);
+      return returnValue;
+    };
+  };
+}
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(middleware1, middleware2)),
+);
 
 export default store;
