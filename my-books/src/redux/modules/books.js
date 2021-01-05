713118fbdd5e03f58487fb27afaa841d479b8c83
NaraBaek <nara130825@gmail.com>
2021-01-05 15:42:51 +0900

    redux-actions라이브러리 사용

--- my-books/src/redux/modules/books.js
+++ my-books/src/redux/modules/books.js
@@ -1,60 +1,62 @@
 import BookService from '../../services/BookService';
 import { call, put, takeEvery, delay, select } from 'redux-saga/effects';
+import { createAction, createActions, handleActions } from 'redux-actions';
 
 // namespace
-const namespace = 'my-books/books';
+const prefix = 'my-books/books';
 
-// action types
-export const BOOK_SUCCESS = namespace + '/BOOK_SUCCESS';
-export const BOOK_START = namespace + '/BOOK_START';
-export const BOOK_FAIL = namespace + '/BOOK_FAIL';
+// action creator
+const { start, success, fail } = createActions('START', 'SUCCESS', 'FAIL', {
+  prefix,
+});
 
 // initial state
 const initialState = { books: [], loading: false, error: null };
 
 // reducer
-export default function books(state = initialState, action) {
-  switch (action.type) {
-    case BOOK_SUCCESS:
-      return { books: [...action.books], loading: false, error: null };
-    case BOOK_START:
-      return { ...state, loading: true, error: null };
-    case BOOK_FAIL:
-      return { ...state, loading: false, error: action.error };
-    default:
-      return state;
-  }
-}
+const books = handleActions(
+  {
+    START: (state, action) => ({ ...state, loading: true, error: null }),
+    SUCCESS: (state, action) => ({
+      books: action.payload,
+      loading: false,
+      error: null,
+    }),
+    FAIL: (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialState,
+  { prefix },
+);
+export default books;
 
-// action creator
-const bookSuccess = (books) => ({ type: BOOK_SUCCESS, books });
-const bookStart = () => ({ type: BOOK_START });
-const bookFail = (error) => ({ type: BOOK_FAIL, error });
-
-const BOOK_SAGA = namespace + '/BOOK_SAGA';
+const BOOKS_SAGA = prefix + '/BOOK_SAGA';
 
 // saga
 function* getBooksSaga() {
   // side effect 처리 로직
   try {
-    yield put(bookStart());
+    yield put(start());
     yield delay(2000);
 
     const token = yield select((state) => state.auth.token);
     const books = yield call(BookService.getBooks, token);
 
-    yield put(bookSuccess(books));
+    yield put(success(books));
   } catch (error) {
     console.log(error);
-    yield put(bookFail(error));
+    yield put(fail(error));
   }
 }
 
 // 사가에 넣을 액션 생성함수
-export const getBooksSagaStart = () => ({ type: BOOK_SAGA });
+export const getBooksSagaStart = createAction(BOOKS_SAGA);
 
 // 사가 액션 생성자 함수 dispatch시 getBooksSaga함수로직이 실행되기위해 등록을해야됨
 // 등록
 export function* booksSaga() {
-  yield takeEvery(BOOK_SAGA, getBooksSaga); // BOOK_SAGA타입의 액션 dispatch시 getBooksSaga로직 실행
+  yield takeEvery(BOOKS_SAGA, getBooksSaga); // BOOK_SAGA타입의 액션 dispatch시 getBooksSaga로직 실행
 }
