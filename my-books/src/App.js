977f9b24767a37dddcbda9694656870ea799f41c
NaraBaek <nara130825@gmail.com>
2021-01-05 18:47:25 +0900

    createPortal 사용

--- my-books/src/App.js
+++ my-books/src/App.js
@@ -12,6 +12,8 @@ import NotFound from './pages/NotFound';
 import create from './redux/create';
 import { createBrowserHistory } from 'history';
 import { ConnectedRouter } from 'connected-react-router';
+import { useState } from 'react';
+import Modal from './components/Modal';
 
 // 1. 히스토리를 생성
 const history = createBrowserHistory();
@@ -19,8 +21,26 @@ const history = createBrowserHistory();
 const store = create(history);
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
   return (
     <ErrorBoundary FallbackComponent={Error}>
+      {visible && (
+        <Modal>
+          <div
+            style={{
+              width: '100vw',
+              height: '100vh',
+              background: 'rgba(0,0,0,0.5)',
+            }}
+            onClick={hide}
+          >
+            나는 모달이다.
+          </div>
+        </Modal>
+      )}
+      <button onClick={show}>모달 오픈</button>
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <Switch>
---
+++ my-books/src/components/Modal.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+const Modal = ({ children }) =>
+  ReactDOM.createPortal(children, document.querySelector('#modal'));
+
+export default Modal;
+
+//<Modal><div>모달</div></Modal>
